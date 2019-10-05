66a714abe13e016fcb9b4102b8f493f7a1e6bf69
Bagus Aji Santoso <lobothijau@gmail.com>
2019-10-05 22:41:07 +0700

    new commit

--- project/__init__.py
+++ project/__init__.py
@@ -1,18 +1,33 @@
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
+import os
+
+from flask import Flask  # new
+from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__)
+# instantiate the db
+db = SQLAlchemy()
 
-api = Api(app)
 
-app.config.from_object('project.config.DevelopmentConfig')
+# new
+def create_app(script_info=None):
 
-class UsersPing(Resource):
-	def get(self):
-		return {
-			'status': 'success',
-			'message': 'pong!'
-		}
+    # instantiate the app
+    app = Flask(__name__)
 
-api.add_resource(UsersPing, '/users/ping')
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
